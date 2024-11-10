662f9ce62ba92c007e0efde1fa901a89f2b99401
gsurrel <1993113+gsurrel@users.noreply.github.com>
2024-11-10 11:05:00 +0100

    Load source and destination files

--- lib/data_access_layer/data_sources/data_source.dart
+++ lib/data_access_layer/data_sources/data_source.dart
@@ -1,9 +1,10 @@
 // Define an abstract class for a data source
 
-import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 import 'package:tao_cat/data_access_layer/document_model.dart';
 
 @immutable
 abstract interface class DataSource {
+  @useResult
   Future<DocumentModel> fetchDocument(String id);
 }
